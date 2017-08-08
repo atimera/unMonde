8b5a98a2f63ae46395c0ccbdb68f7ddb29a264ef
laenzo <amdiatoutimera@hotmail.fr>
2017-08-08 21:09:18 +0200

    Calcule de la surface d'une zone, et de l'agréabilité moyenne d'une zone

--- model.py
+++ model.py
@@ -51,6 +51,7 @@ class Zone:
     MAX_LATITUDE_DEGREES = 90 #latitude maximale
     WIDTH_DEGREES = 1 # l'espacement horizontal
     HEIGHT_DEGREES = 1 # l'espacement verticale
+    EARTH_RADIUS_KILOMETERS = 6371 # le rayon de la terre en kilometre
 
     def __init__(self, corner1, corner2):
         """ le constructeur """
@@ -102,9 +103,31 @@ class Zone:
         return len(self.inhabitants)
 
 
+    # pour convertir une valeur en radian en Km, on la multiplie par le rayon de la terre
+    @property
+    def width(self):
+        """ Convertit une valeur en radian en kilometre
+            en valeur absolue """
+        return abs(self.corner1.longitude - self.corner2.longitude) * self.EARTH_RADIUS_KILOMETERS
+    @property
+    def height(self):
+        """ Convertit une valeur en radian en kilometre
+            en valeur absolue """
+        return abs(self.corner1.latitude - self.corner2.latitude) * self.EARTH_RADIUS_KILOMETERS
+    @property
+    def area(self):
+        """ Calcule la surface d'une zone : height * width """
+        return self.height * self.width
 
-
-
+    def average_agreeableness(self):
+        """ Calcule l'agréabilité moyenne d'une zone """
+        if not self.inhabitants:
+            return 0 # la valeur neutre de l'agréabilité
+        #agreeableness = []
+        #for inhabitant in self.inhabitants:
+        #    agreeableness.append(inhabitant.agreeableness)
+        #return sum(agreeableness) / self.population
+        return sum([inhabitant.agreeableness for inhabitant in self.inhabitants]) / self.population
 
 
 
@@ -120,7 +143,7 @@ def main():
         agent = Agent(position, **agent_attributes)
         zone = Zone.find_zone_that_contains(position)
         zone.add_inhabitant(agent)
-        print(zone.population)
+        print(zone.average_agreeableness())
         
 
 main()
